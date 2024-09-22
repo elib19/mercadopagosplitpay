d53494695cf5c07ae81dfbd6c463e31d7f8cc387
elib19 <143918479+elib19@users.noreply.github.com>
2024-09-22 16:24:26 -0300

    Update helper.php

--- includes/helper.php
+++ includes/helper.php
@@ -16,6 +16,11 @@ class MP_Split_Helper {
         return (int) get_option( 'mp_application_fee', 10 );  // Retorna 10% por padrão
     }
 
+    // Função para obter a chave PIX do vendedor
+    public static function get_vendor_pix_key($vendor_id) {
+        return get_user_meta($vendor_id, 'mp_pix_key', true); // Assumindo que você armazena a chave PIX no meta do usuário
+    }
+
     // Função auxiliar para realizar uma chamada à API do Mercado Pago
     public static function call_mp_api( $url, $method = 'GET', $data = array() ) {
         $access_token = self::get_mp_access_token();
