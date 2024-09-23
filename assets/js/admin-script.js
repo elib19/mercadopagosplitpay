6632151ef89bb321baf1742d4298881a0d883121
elib19 <143918479+elib19@users.noreply.github.com>
2024-09-23 15:03:23 -0300

    Create admin-script.js

---
+++ assets/js/admin-script.js
@@ -0,0 +1,9 @@
+// Script para o painel de administração do Mercado Pago Split
+
+(function($) {
+    $(document).ready(function() {
+        $('#mp-split-save-settings').on('click', function() {
+            alert('Configurações salvas com sucesso!');
+        });
+    });
+})(jQuery);
