18df6c66cf20e42e9cfa7b7c89277b60588d3721
elib19 <143918479+elib19@users.noreply.github.com>
2024-09-23 15:24:36 -0300

    Update install.php

--- install.php
+++ install.php
@@ -1,19 +1,14 @@
 <?php
 /**
- * Script de instalação do Mercado Pago Split Payment
+ * Funções de instalação do plugin
  */
 
 function mp_split_install() {
-    // Define opções padrão
-    $default_options = array(
+    // Aqui você pode adicionar opções padrão ao ativar o plugin, se necessário
+    add_option('mp_split_settings', array(
         'mp_split_app_id' => '',
         'mp_split_client_secret' => '',
         'mp_split_redirect_uri' => ''
-    );
-
-    if (!get_option('mp_split_settings')) {
-        add_option('mp_split_settings', $default_options);
-    }
+    ));
 }
-
 register_activation_hook(__FILE__, 'mp_split_install');
