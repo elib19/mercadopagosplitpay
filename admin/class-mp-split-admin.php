cbcd7227c50566cf614ad479d902441f4439ec18
elib19 <143918479+elib19@users.noreply.github.com>
2024-09-22 23:20:51 -0300

    Update class-mp-split-admin.php

--- admin/class-mp-split-admin.php
+++ admin/class-mp-split-admin.php
@@ -1,36 +1,35 @@
 <?php
 
-class MP_Split_Admin {
-
-    public function __construct() {
-        add_action( 'admin_menu', array( $this, 'mp_add_admin_menu' ) );
-        add_action( 'admin_init', array( $this, 'mp_register_settings' ) );
+class MP_Split_Admin
+{
+    public function __construct()
+    {
+        add_action('admin_menu', array($this, 'mp_add_admin_menu'));
+        add_action('admin_init', array($this, 'mp_register_settings'));
     }
 
-    // Adicionar menu ao painel de administração
-    public function mp_add_admin_menu() {
+    public function mp_add_admin_menu()
+    {
         add_menu_page(
-            __( 'Mercado Pago Split', 'mercado-pago-split-wcfm' ),
-            __( 'Mercado Pago Split', 'mercado-pago-split-wcfm' ),
+            __('Mercado Pago Split', 'mercado-pago-split-wcfm'),
+            __('Mercado Pago Split', 'mercado-pago-split-wcfm'),
             'manage_options',
             'mp-split-settings',
-            array( $this, 'mp_settings_page' ),
+            array($this, 'mp_settings_page'),
             'dashicons-admin-settings'
         );
     }
 
-    // Registrar configurações
-    public function mp_register_settings() {
-        register_setting( 'mp-split-settings-group', 'mp_access_token' );
-        register_setting( 'mp-split-settings-group', 'mp_application_fee' );
-        register_setting( 'mp-split-settings-group', 'mp_pix_key' ); // Nova configuração para chave PIX
+    public function mp_register_settings()
+    {
+        register_setting('mp-split-settings-group', 'mp_access_token');
+        register_setting('mp-split-settings-group', 'mp_application_fee');
     }
 
-    // Renderizar página de configurações
-    public function mp_settings_page() {
-        require_once plugin_dir_path( __FILE__ ) . 'views/admin-settings-page.php';
+    public function mp_settings_page()
+    {
+        require_once plugin_dir_path(__FILE__) . 'views/admin-settings-page.php';
     }
 }
 
-// Inicializar a classe de administração
 new MP_Split_Admin();
