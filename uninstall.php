6256a33ba709e172738b1bacdc9f6ee5e134398a
elib19 <143918479+elib19@users.noreply.github.com>
2024-09-23 15:25:24 -0300

    Update uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -1,15 +1,9 @@
 <?php
 /**
- * Script de desinstalação do Mercado Pago Split Payment
+ * Funções de desinstalação do plugin
  */
 
-if (!defined('WP_UNINSTALL_PLUGIN')) {
-    exit;
-}
-
 function mp_split_uninstall() {
-    // Remove configurações do plugin
     delete_option('mp_split_settings');
 }
-
-mp_split_uninstall();
+register_uninstall_hook(__FILE__, 'mp_split_uninstall');
