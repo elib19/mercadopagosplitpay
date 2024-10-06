16b8ed388d0b91bb5d4a71bf2795364ed2ed05cf
elib19 <143918479+elib19@users.noreply.github.com>
2024-10-06 11:26:51 -0300

    Update install.php

--- install.php
+++ install.php
@@ -1,14 +1,18 @@
 <?php
-/**
- * Funções de instalação do plugin
- */
-
 function mp_split_install() {
-    // Aqui você pode adicionar opções padrão ao ativar o plugin, se necessário
-    add_option('mp_split_settings', array(
-        'mp_split_app_id' => '',
-        'mp_split_client_secret' => '',
-        'mp_split_redirect_uri' => ''
-    ));
+    global $wpdb;
+
+    $table_name = $wpdb->prefix . 'mp_split_vendors';
+    $charset_collate = $wpdb->get_charset_collate();
+
+    $sql = "CREATE TABLE $table_name (
+        id mediumint(9) NOT NULL AUTO_INCREMENT,
+        vendor_id bigint(20) NOT NULL,
+        access_token varchar(255) NOT NULL,
+        sponsor_id varchar(255),
+        PRIMARY KEY (id)
+    ) $charset_collate;";
+
+    require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+    dbDelta( $sql );
 }
-register_activation_hook(__FILE__, 'mp_split_install');
