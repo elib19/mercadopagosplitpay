7aa1770cd8ea802e2ba13e5b3deef9742917a34e
elib19 <143918479+elib19@users.noreply.github.com>
2024-10-06 11:35:49 -0300

    Delete assets/js/admin-script.js

--- assets/js/admin-script.js
+++
@@ -1,8 +0,0 @@
-// Script para o painel de administração do Mercado Pago Split
-(function($) {
-    $(document).ready(function() {
-        $('#mp-split-save-settings').on('click', function() {
-            alert('Configurações salvas com sucesso!');
-        });
-    });
-})(jQuery);
