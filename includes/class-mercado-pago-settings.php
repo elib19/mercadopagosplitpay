a20b8cacfdac511de846c11595ac7435dd908110
elib19 <143918479+elib19@users.noreply.github.com>
2024-10-06 14:54:41 -0300

    Update class-mercado-pago-settings.php

--- includes/class-mercado-pago-settings.php
+++ includes/class-mercado-pago-settings.php
@@ -1,41 +1,77 @@
 <?php
 
+// Evita acesso direto ao arquivo
+defined('ABSPATH') || exit;
+
 class Mercado_Pago_Settings {
-    public static function init() {
-        add_action('admin_menu', array(__CLASS__, 'add_admin_menu'));
-        add_action('admin_init', array(__CLASS__, 'register_settings'));
+    public function __construct() {
+        // Adiciona ações para registrar e renderizar as configurações
+        add_action('admin_init', array($this, 'register_settings'));
     }
 
-    public static function add_admin_menu() {
-        add_menu_page(
-            'Configurações Mercado Pago',
-            'Configurações Mercado Pago',
-            'manage_options',
-            'mercado-pago-settings',
-            array(__CLASS__, 'settings_page')
+    /**
+     * Registra as configurações do Mercado Pago
+     */
+    public function register_settings() {
+        register_setting('mercado_pago_options_group', 'mercado_pago_settings');
+
+        add_settings_section(
+            'mercado_pago_section',
+            __('Configurações do Mercado Pago', 'mercado-pago-split'),
+            array($this, 'settings_section_callback'),
+            'mercado_pago_settings'
+        );
+
+        add_settings_field(
+            'access_token',
+            __('Access Token', 'mercado-pago-split'),
+            array($this, 'render_access_token_field'),
+            'mercado_pago_settings',
+            'mercado_pago_section'
+        );
+
+        add_settings_field(
+            'public_key',
+            __('Public Key', 'mercado-pago-split'),
+            array($this, 'render_public_key_field'),
+            'mercado_pago_settings',
+            'mercado_pago_section'
+        );
+
+        add_settings_field(
+            'sandbox',
+            __('Modo Sandbox', 'mercado-pago-split'),
+            array($this, 'render_sandbox_field'),
+            'mercado_pago_settings',
+            'mercado_pago_section'
         );
     }
 
-    public static function register_settings() {
-        register_setting('mercado_pago_group', 'mercado_pago_settings');
+    public function settings_section_callback() {
+        echo __('Configure as opções abaixo para integrar o Mercado Pago.', 'mercado-pago-split');
     }
 
-    public static function settings_page() {
+    public function render_access_token_field() {
+        $options = get_option('mercado_pago_settings');
         ?>
-        <div class="wrap">
-            <h1><?php _e('Configurações Mercado Pago', 'woocommerce'); ?></h1>
-            <form method="post" action="options.php">
-                <?php settings_fields('mercado_pago_group'); ?>
-                <?php $options = get_option('mercado_pago_settings'); ?>
-                <table class="form-table">
-                    <tr valign="top">
-                        <th scope="row"><?php _e('Access Token', 'woocommerce'); ?></th>
-                        <td><input type="text" name="mercado_pago_settings[access_token]" value="<?php echo esc_attr($options['access_token']); ?>" /></td>
-                    </tr>
-                </table>
-                <?php submit_button(); ?>
-            </form>
-        </div>
+        <input type="text" name="mercado_pago_settings[access_token]" value="<?php echo esc_attr($options['access_token'] ?? ''); ?>" />
+        <?php
+    }
+
+    public function render_public_key_field() {
+        $options = get_option('mercado_pago_settings');
+        ?>
+        <input type="text" name="mercado_pago_settings[public_key]" value="<?php echo esc_attr($options['public_key'] ?? ''); ?>" />
+        <?php
+    }
+
+    public function render_sandbox_field() {
+        $options = get_option('mercado_pago_settings');
+        ?>
+        <input type="checkbox" name="mercado_pago_settings[sandbox]" value="1" <?php checked(1, isset($options['sandbox']) ? $options['sandbox'] : 0); ?> />
         <?php
     }
 }
+
+// Inicializa a classe
+new Mercado_Pago_Settings();
