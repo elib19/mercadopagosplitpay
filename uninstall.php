8b8173944f5d0924604310ed7ea9507a62b07c27
elib19 <143918479+elib19@users.noreply.github.com>
2024-10-06 13:44:20 -0300

    Update uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -1,9 +1,20 @@
 <?php
 
-if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
-    exit; // Impede acesso direto
+// Evita acesso direto ao arquivo
+defined('ABSPATH') || exit;
+
+/**
+ * Função para desinstalar o plugin
+ */
+function mercado_pago_uninstall() {
+    // Remove opções do banco de dados
+    delete_option('mercado_pago_settings');
+    
+    // Se necessário, adicione código para remover tabelas do banco de dados aqui
+    global $wpdb;
+    $table_name = $wpdb->prefix . 'mercado_pago_transactions';
+    $wpdb->query("DROP TABLE IF EXISTS $table_name");
 }
 
-global $wpdb;
-$table_name = $wpdb->prefix . 'mercado_pago_transactions';
-$wpdb->query("DROP TABLE IF EXISTS $table_name");
+// Aciona a função de desinstalação ao desinstalar o plugin
+register_uninstall_hook(__FILE__, 'mercado_pago_uninstall');
