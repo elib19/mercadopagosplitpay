f34afd0db3b66015cc67c1e98cffbce980b52b5d
elib19 <143918479+elib19@users.noreply.github.com>
2024-10-06 11:59:09 -0300

    Update install.php

--- includes/install.php
+++ includes/install.php
@@ -1,16 +1,18 @@
 <?php
-function mp_split_install() {
+function mp_split_create_tables() {
     global $wpdb;
 
-    $table_name = $wpdb->prefix . 'mp_split_vendors';
+    $table_name = $wpdb->prefix . 'mp_split_transactions';
     $charset_collate = $wpdb->get_charset_collate();
 
     $sql = "CREATE TABLE $table_name (
         id mediumint(9) NOT NULL AUTO_INCREMENT,
+        order_id bigint(20) NOT NULL,
         vendor_id bigint(20) NOT NULL,
-        access_token varchar(255) NOT NULL,
-        sponsor_id varchar(255),
-        PRIMARY KEY (id)
+        amount decimal(10,2) NOT NULL,
+        status varchar(20) NOT NULL,
+        payment_date datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
+        PRIMARY KEY  (id)
     ) $charset_collate;";
 
     require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
