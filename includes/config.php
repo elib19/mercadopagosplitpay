ea4e5a5f4dc5378b9799a63e57826418cdf599f5
elib19 <143918479+elib19@users.noreply.github.com>
2024-11-09 14:47:38 -0300

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,11 +1,8 @@
 <?php
 // includes/config.php
 
-if (!defined('ABSPATH')) {
-    exit; // Exit if accessed directly.
-}
-
-// Define constants.
-define('MERCADO_PAGO_CLIENT_ID', 'YOUR_CLIENT_ID');
-define('MERCADO_PAGO_CLIENT_SECRET', 'YOUR_CLIENT_SECRET');
-define('MERCADO_PAGO_REDIRECT_URI', 'https://juntoaqui.com.br?store-setup=yes&step=payment/callback');
+define('CLIENT_ID', 'YOUR_CLIENT_ID');
+define('CLIENT_SECRET', 'YOUR_CLIENT_SECRET');
+define('REDIRECT_URI', 'https://juntoaqui.com.br?store-setup=yes&step=payment/oauth/callback');
+define('AUTH_URL', 'https://auth.mercadopago.com/authorization');
+define('TOKEN_URL', 'https://api.mercadopago.com/oauth/token');
