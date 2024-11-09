0d93562b3fb4b267a92ecb3a10b0de8a6de24ef6
elib19 <143918479+elib19@users.noreply.github.com>
2024-11-09 14:29:28 -0300

    Update and rename mercado-pago-split.php to wcfm-mercado-pago-integration.php

---
+++ wcfm-mercado-pago-integration.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Plugin Name: Mercado Pago Integration
+ * Plugin URI: https://juntoaqui.com.br/
+ * Description: Integração do Mercado Pago com WooCommerce e WCFM Marketplace.
+ * Version: 1.0
+ * Author: Eli Silva
+ * Author URI: https://juntoaqui.com.br/
+ * Text Domain: mercado-pago-split
+ * Domain Path: /languages
+ * License: GPL2
+ */
+
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly.
+}
+
+// Include necessary files.
+include_once plugin_dir_path(__FILE__) . 'includes/class-wcfm-mercado-pago-gateway.php';
+include_once plugin_dir_path(__FILE__) . 'includes/class-wcfm-mercado-pago-auth.php';
+
+// Initialize the plugin.
+add_action('plugins_loaded', 'wcfm_mercado_pago_init');
+function wcfm_mercado_pago_init() {
+    if (class_exists('WCFMmp')) {
+        new WCFMmp_Gateway_Mercado_Pago();
+        new WCFM_Mercado_Pago_Auth();
+    }
+}
