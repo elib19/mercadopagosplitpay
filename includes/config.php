fab8e6ad7b8ab69d6a75025652a00cb8cdb62fa5
elib19 <143918479+elib19@users.noreply.github.com>
2024-11-23 15:32:10 -0300

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,9 +1,10 @@
 <?php
 // includes/config.php
 
+// URL para autenticação e troca de token
 define('AUTH_URL', 'https://auth.mercadopago.com/authorization');
 define('TOKEN_URL', 'https://api.mercadopago.com/oauth/token');
 
-// Redirecionamento padrão configurável
+// URI de redirecionamento após a autenticação
 define('REDIRECT_URI', admin_url('admin-post.php?action=mp_oauth_callback'));
 ?>
