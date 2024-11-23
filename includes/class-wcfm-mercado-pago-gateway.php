b706d4d9318e5655fd97400806e01e66427ce385
elib19 <143918479+elib19@users.noreply.github.com>
2024-11-23 15:31:16 -0300

    Update class-wcfm-mercado-pago-gateway.php

--- includes/class-wcfm-mercado-pago-gateway.php
+++ includes/class-wcfm-mercado-pago-gateway.php
@@ -3,15 +3,19 @@
 
 class WCFMmp_Gateway_Mercado_Pago {
     public function __construct() {
+        // Adiciona o método de pagamento
         add_filter('wcfm_marketplace_withdrwal_payment_methods', [$this, 'add_gateway']);
+        // Adiciona configurações ao método de pagamento
         add_filter('wcfm_marketplace_settings_fields_withdrawal_payment_keys', [$this, 'add_settings'], 50, 2);
     }
 
+    // Adiciona o Mercado Pago como método de pagamento
     public function add_gateway($payment_methods) {
         $payment_methods['mercado_pago'] = 'Mercado Pago';
         return $payment_methods;
     }
 
+    // Adiciona as configurações para o Mercado Pago
     public function add_settings($payment_keys, $wcfm_withdrawal_options) {
         $gateway_slug = 'mercado_pago';
         $payment_keys = array_merge($payment_keys, [
